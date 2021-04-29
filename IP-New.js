45302fcd1dab0882e9fdb949402fc5f2f67e42cc
QiongBan <50990433+QiongBan@users.noreply.github.com>
2021-04-29 19:23:37 +0800

    Update IP-New.js

--- IP-New.js
+++ IP-New.js
@@ -1,13 +1,12 @@
 if ($response.statusCode != 200) {
-  $done(Null);
+  $done(null);
 }
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = obj['country'];
+var ip = obj['ip'];
+var title = obj['country_name'];
 var subtitle = obj['city'] + ' ' + obj['isp'];
-var ip = obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
-
+var description = "Country" + ":" + obj['country_name'] + '\n' + "City" + ":" + obj['city'] + '\n' + "Carrier" + ":" + obj['isp'] + '\n' + "Organization" + ":" + obj['organization'];
 
 $done({title, subtitle, ip, description});
