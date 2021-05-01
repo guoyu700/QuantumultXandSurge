9c0e48875dfd9eeccd28a24f74e45e26821040e9
QiongBan <50990433+QiongBan@users.noreply.github.com>
2021-05-01 13:02:06 +0800

    Update IP-New.js

--- IP-New.js
+++ IP-New.js
@@ -3,10 +3,10 @@ if ($response.statusCode != 200) {
 }
 
 var body = $response.body;
-var obj = JSON.parse(body)['data'];
+var obj = JSON.parse(body);
 var ip = obj['addr'];
-var title = obj['country'];
+var title = obj['country_name'];
 var subtitle = obj['city'] + ' ' + obj['isp'];
-var description = "Country" + ":" + obj['country'] + '\n' + "City" + ":" + obj['city'] + '\n' + "Carrier" + ":" + obj['isp'];
+var description = "Country" + ":" + obj['country_name'] + '\n' + "City" + ":" + obj['city'] + '\n' + "Carrier" + ":" + obj['isp'];
 
 $done({title, subtitle, ip, description});
