6b6946239cc7025a5cfc7d33e69a9c23c64c8c56
QiongBan <50990433+QiongBan@users.noreply.github.com>
2021-05-01 13:55:48 +0800

    Update IP-New.js

--- IP-New.js
+++ IP-New.js
@@ -4,11 +4,10 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var timezone = JSON.parse(body)["timezone"];
-var flag = JSON.parse(body)["flag"];
-var ip = obj['ip_address'];
-var title = flag['emoji'] + ''+ obj['country'];
+var timezone = JSON.parse(body)["time_zone"];
+var ip = obj['ip'];
+var title = obj['country_name'];
 var subtitle = obj['country_code'];
-var description = "Country: " + obj['country'] + '\n' + "Timezone: " + timezone['name'];
+var description = "Country: " + obj['country_name'] + '\n' + "Timezone: " + timezone['name'] + '\n' + "IP: " + obj['ip'];
 
 $done({title, subtitle, ip, description});
